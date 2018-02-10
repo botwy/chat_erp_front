5c15853f5a5f656cbf65ff9a1d09bceec2cd088c
botwy <botwy@mail.ru>
2018-02-10 19:44:13 +0300

    init redux

--- components/LoginWidget.js
+++ components/LoginWidget.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import style from './Style';
 import PropTypes from 'prop-types';
+import axios from "axios";
+import {connect} from 'react-redux';
 
-const LoginWidget=({loginName,handleChangeMsg,handleClickLoginButton})=>(
+//const LoginWidget=({loginName,handleChangeMsg,handleClickLoginButton})=>(
+const LoginWidget=(props)=>(
     <div style={style.column}>
         <p style={style.row}>Введите ваше имя: </p>
         <div style={style.row}>
-            <input style={style.inputMessage} value={loginName} onChange={handleChangeMsg}></input>
-            <button style={style.buttons} onClick={handleClickLoginButton}>Войти в чат</button>
+            <input style={style.inputMessage} value={props.msgForSend} onChange={props.handleChangeMsg}></input>
+            <button style={style.buttons} onClick={props.handleClickLoginButton}>Войти в чат</button>
         </div>
     </div>
 );
@@ -18,4 +21,10 @@ LoginWidget.propTypes = {
     handleClickLoginButton: PropTypes.func.isRequired
 };
 
-export default LoginWidget;
+//export default LoginWidget;
+
+export default connect(
+    store => ({msgForSend: store.msgForSend}),
+    (dispatch,props) => ({ handleChangeMsg: (event) => dispatch({type:'CHANGEMSG', msgForSend:event.target.value}),
+        handleClickLoginButton:() => dispatch({type:'LOGIN', senderName:props.msgForSend}) })
+)(LoginWidget);
--- main.js
+++ main.js
@@ -2,7 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import axios from "axios";
+
+
+const reducer = (state, action) => {
+    if (action.type == 'LOGIN'){
+            //? так плохо?
+            //const name = document.getElementById("login_input").value;
+        alert("try request to server");
+            axios.get('http://localhost:8080/chat/login?name='+action.senderName)
+                .then((response) => {
+                    if (response.data===false) {
+                        alert("Введите другое имя!");
+                        return Object.assign({}, state, {msgForSend: ''});
+                    }
+                    if (response.data===true) {
+                        alert("O.K.");
+                        return Object.assign({}, state, {login: true, senderName: action.senderName, msgForSend: ''});
+                    }
+
+                })
+                .catch((errror) => console.error(errror));
+        return Object.assign({}, state, {senderName: action.senderName});
+    }
+
+    if (action.type == 'CHANGEMSG'){
+        return Object.assign({}, state, {msgForSend: action.msgForSend});
+    }
+    return state;
+};
+
+const store = createStore(reducer, {login:false, msgForSend:'', senderName: ''});
+
 ReactDOM.render(
-  <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
